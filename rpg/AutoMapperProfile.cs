26fc268ad052c6841e249941dcb349a8615a6f35
kofeex <speklja996@gmail.com>
2023-02-24 15:34:14 +0100

    Added AddCharacter and GetCharacter DTOs and Automappers for them

---
+++ rpg/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using rpg.Dtos.Character;
+using rpg.Models;
+
+namespace rpg
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
--- rpg/Controllers/CharacterController.cs
+++ rpg/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rpg.Dtos.Character;
 using rpg.Models;
 using rpg.Services.CharacterService;
 
@@ -17,19 +18,19 @@ namespace rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         { 
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
