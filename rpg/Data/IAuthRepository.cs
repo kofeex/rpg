7bfa44f5c30f0fd2d62ca9c6fd298e9dbe07ad03
kofeex <speklja996@gmail.com>
2023-02-27 15:08:16 +0100

    Added AuthRepository and AuthController for logging in and registering

---
+++ rpg/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using rpg.Models;
+
+namespace rpg.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
