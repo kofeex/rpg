d53ddd3bf510cc60efe8f6cca1d92a69b4a05283
kofeex <speklja996@gmail.com>
2023-02-28 16:59:09 +0100

    Added SkillAttack method to Fight Service and Contoller

--- rpg/Services/FightService/IFightService.cs
+++ rpg/Services/FightService/IFightService.cs
@@ -6,5 +6,6 @@ namespace rpg.Services.FightService
     public interface IFightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
     }
 }
